7120507b6801e16aababda4a7d5fec8b73cbd0b8
nadaabdelmaboud <42664649+nadaabdelmaboud@users.noreply.github.com>
2021-08-05 17:44:54 +0200

    autowire decorator v1

--- Flask-Restx/app-v2.py
+++ Flask-Restx/app-v2.py
@@ -1,5 +1,9 @@
 from flask import Flask, Blueprint, jsonify, request
-from flask_restx import Api, Resource, fields
+from flask_restx import Api, Resource, fields, marshal_with, reqparse
+from typing import List
+from functools import wraps
+
+from werkzeug import Request
 from auth import auth, encode_jwt
 
 
@@ -57,50 +61,88 @@ class CourseDAO(object):
         CourseDAO.courses.remove(course)
 
 
-@courses_ns.route('/')
+def autowire_decorator(func):
+    params = func.__annotations__
+
+    @wraps(func)
+    def path_wrapper(*args, **kwargs):
+        return args[0]
+    resource = path_wrapper.__qualname__.split('.')[0]
+    # todo-path params
+
+    # return_params=params['return']
+    #model = get_model(return_params)
+    params.pop('return', None)
+
+    #parser = get_parser(params)
+
+    #test_example
+    model = api.model('Course', {
+        'name': fields.String(required=True, description='The course name'),
+        'duration': fields.Integer(required=True, description='The course duration'),
+        'teachers': fields.List(fields.String, description='The course teachers'),
+    })
+    parser = reqparse.RequestParser()
+    parser.add_argument('name', type=str, help='The course name')
+    parser.add_argument('duration', type=int, help='The course duration')
+    parser.add_argument('teachers', type=List[str], help='The course teachers')
+
+    @wraps(func)
+    @api.expect(parser)
+    @api.marshal_with(model)
+    def wrapper(*args, **kwargs):
+        args_parser = parser.parse_args()
+        return func(*args, *args_parser.values(), **kwargs)
+    return wrapper
+
+
+@ api.route('/')
 class CourseList(Resource):
-    @courses_ns.doc('list_courses', security='apikey')
-    @auth
-    @courses_ns.marshal_list_with(course)
+    @ courses_ns.doc('list_courses', security='apikey')
+    # @auth
+    @ courses_ns.marshal_list_with(course)
     def get(self):
         return CourseDAO.courses
 
-    @courses_ns.doc('create_course', security='apikey')
-    @auth
-    @courses_ns.expect(course)
-    @courses_ns.marshal_with(course, code=201)
-    def post(self):
-        return CourseDAO.create(api.payload), 201
+    @ courses_ns.doc('create_course', security='apikey')
+    @ autowire_decorator
+    def post(self, name: str, duration: int, teachers: List[str]) -> course:
+        course_data = {
+            'name': name,
+            'duration': duration,
+            'teachers': teachers
+        }
+        return CourseDAO.create(course_data), 201
 
 
-@courses_ns.route('/<int:id>')
+@ api.route('/<int:id>')
 class Course(Resource):
-    @courses_ns.doc('get_course', security='apikey')
-    @auth
-    @courses_ns.marshal_with(course)
+    @ courses_ns.doc('get_course', security='apikey')
+    # @auth
+    # @courses_ns.marshal_with(course)
     def get(self, id):
         return CourseDAO.get(id)
 
-    @courses_ns.doc('update_course', security='apikey')
-    @auth
-    @courses_ns.expect(course)
-    @courses_ns.marshal_with(course)
+    @ courses_ns.doc('update_course', security='apikey')
+    # @auth
+    @ courses_ns.expect(course)
+    @ courses_ns.marshal_with(course)
     def put(self, id):
         return CourseDAO.update(id, api.payload)
 
-    @courses_ns.doc('delete_course', security='apikey')
-    @auth
-    @courses_ns.response(204, 'Course deleted')
+    @ courses_ns.doc('delete_course', security='apikey')
+    # @auth
+    @ courses_ns.response(204, 'Course deleted')
     def delete(self, id):
         CourseDAO.delete(id)
         return '', 204
 
 
-@auth_ns.route('/login')
+@ auth_ns.route('/login')
 class Login(Resource):
-    @auth_ns.doc('login')
-    @auth_ns.response(200, 'Success')
-    @auth_ns.expect({'username': fields.String, 'password': fields.String})
+    @ auth_ns.doc('login')
+    @ auth_ns.response(200, 'Success')
+    @ auth_ns.expect({'username': fields.String, 'password': fields.String})
     def post(self):
         username = api.payload['username']
         password = api.payload['password']
