da9e0140db73f3f6d4814c992c5616a0a1598bbb
omargamal253 <omargamal253@gmail.com>
2021-08-09 08:44:49 +0200

    update get parser method to support List and Literal types

--- extensions/get_parser.py
+++ extensions/get_parser.py
@@ -1,7 +1,5 @@
-from typing import Callable
+import ast
 from flask_restful import reqparse
-import inspect
-from fastapi import Query, Body, Cookie, Header, Path
 
 
 def get_parser(signature, parameters) -> reqparse:
@@ -26,50 +24,41 @@ def get_parser(signature, parameters) -> reqparse:
 
         elif str(param).find('Path') != -1:
             continue
+        else:
+            location = 'args'
 
-        # print("name : {}".format(param.name))
-        # print("param : {}".format(param))
-        # print("location : {}".format(location))
-        # print("default : {}".format(param.default))
-        # print("annotation : {}\n".format(parameters[str(param.name)].annotation))
+        annotation = parameters[str(param.name)].annotation
 
-        parser.add_argument(str(param.name), type=parameters[str(param.name)].annotation,
-                            location=location)
+        if str(annotation).find('typing.List[str]') != -1:
+            parser.add_argument(str(param.name), type=str, action='append',
+                                location=location)
+        elif str(annotation).find('typing.List[int]') != -1:
+            parser.add_argument(str(param.name), type=int, action='append',
+                                location=location)
+
+        elif str(annotation).find('typing.List[float]') != -1:
+            parser.add_argument(str(param.name), type=float, action='append',
+                                location=location)
+        elif str(annotation).find('typing.Literal') != -1:
+
+            res = get_literal_tuple(str(annotation))
+            if res and len(res) > 0:
+                parser.add_argument(str(param.name), type=type(res[0]), required=True, choices=res,
+                                    location=location)
+
+        else:
+            parser.add_argument(str(param.name), type=parameters[str(param.name)].annotation,
+                                location=location)
 
     return parser
 
-# full example to how get_parser() work.
 
+def get_literal_tuple(annotation):
+    s: int = str(annotation).index('[')
+    e: int = str(annotation).index(']')
+    str_list = str(annotation)[s + 1:e]
 
-'''
-@ns.route('/enroll/<int:id_>')
-class CourseEnroll(Resource):
+    res = ast.literal_eval(str_list)
 
-    #below declare for every param -> name , type , location based on 
-    # Query(None)-> location = 'args'
-    # Body(None) -> location = 'form'
-    # Header(None) -> location = 'header'
-    # Cookie(None) -> location = 'cookies'
-    # Path(None) this means path param
-    # default -> location = 'args'
-    
-    def post(self, id_=Path(None), name: str = Query(None), course_name: str = Body(None),
-             duration: float = Header(None)):
-             
-        signature = inspect.signature(CourseEnroll.post)
-        parameters = dict(signature.parameters)
-        student_args = get_parser(signature, parameters).parse_args() #  <--- get_parser()
-        print(id_)
-        print(student_args["name"])          #get name from Query  
-        print(student_args["course_name"])   #get course_name from Body  
-        print(student_args["duration"])      #get duration from Header  
-
-        student = {
-            "course_id": id_,
-            "name": student_args["name"],
-            "course_name": student_args["course_name"],
-            "duration": student_args["duration"]
-        }
-        students.append(student)
-        return {"Message:": "Successfully Enrolled", "Student": student}, 200
-'''
+    if res:
+        return tuple(res)
