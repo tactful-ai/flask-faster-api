8a8de82f96470cbf6c3119f1f781db4cc236fdbb
nadaabdelmaboud <42664649+nadaabdelmaboud@users.noreply.github.com>
2021-08-10 15:05:59 +0200

    pylint modifications

--- FlaskRestY/flaskresty/autowire_decorator.py
+++ FlaskRestY/flaskresty/autowire_decorator.py
@@ -1,33 +1,38 @@
-from flask_restx import Api
-from functools import wraps
+"""The Autowire decorator is used to
+replace @api.marshal_with decorator and
+@api.expect decorator through supporting MyPy types."""
 import inspect
-from .model_api import create_model
-from .path_param import ExtractPathParams
-from .parser_api import get_parser
+from functools import wraps
+from flask_restx import Api
+from flaskresty.model_api import create_model
+from flaskresty.path_param import ExtractPathParams
+from flaskresty.parser_api import get_parser
 api = Api()
 
 
 def register_api(api_main):
+    """Register The Main App Api"""
     global api
     api = api_main
 
 
 def autowire_decorator(path):
+    """The Autowire Decorator with a path parameter (the url/endpoint)"""
     def decorator(func):
-
+        """The decorator that wraps the function"""
         params_return = func.__annotations__
-        modelName = func.__qualname__.split('.')[0]
+        model_name = func.__qualname__.split('.')[0]
         params_return = params_return.get('return')
-        isPrimitive = False
-        if(params_return == None):
-            api_model = api.model(modelName, {})
+        isprimitive = False
+        if params_return is None:
+            api_model = api.model(model_name, {})
         else:
-            if(type(params_return) != dict):
+            if not isinstance(params_return, dict):
                 params_return = {'data': params_return}
-                isPrimitive = True
+                isprimitive = True
             api_model = create_model(
                 params_return)
-            api_model = api.model(modelName, api_model)
+            api_model = api.model(model_name, api_model)
         path_params = ExtractPathParams(path)
         signature = inspect.signature(func)
         parameters = dict(signature.parameters)
@@ -42,9 +47,8 @@ def autowire_decorator(path):
         @api.marshal_with(api_model)
         def wrapper(*args, **kwargs):
             args_parser = parser.parse_args()
-            if(isPrimitive):
+            if isprimitive:
                 return {'data': func(*args, **args_parser, **kwargs)}
-            else:
-                return func(*args, **args_parser, **kwargs)
+            return func(*args, **args_parser, **kwargs)
         return wrapper
     return decorator
