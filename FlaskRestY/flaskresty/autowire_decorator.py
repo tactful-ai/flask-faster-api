1947191713a9dfcd3b5f6133d9a43ed57b8b45cc
nadaabdelmaboud <42664649+nadaabdelmaboud@users.noreply.github.com>
2021-08-11 13:51:23 +0200

    Update Path Handling

--- FlaskRestY/flaskresty/autowire_decorator.py
+++ FlaskRestY/flaskresty/autowire_decorator.py
@@ -5,7 +5,6 @@ import inspect
 from functools import wraps
 from flask_restx import Api
 from flaskresty.model_api import create_model
-from flaskresty.path_param import ExtractPathParams
 from flaskresty.parser_api import get_parser
 api = Api()
 
@@ -30,45 +29,39 @@ def get_params_description(doc):
     return params_description
 
 
-def autowire_decorator(path):
-    """The Autowire Decorator with a path parameter (the url/endpoint)"""
-    def decorator(func):
-        """The decorator that wraps the function"""
-        print(api.resources)
-        params_return = func.__annotations__
-        model_name = func.__qualname__.split('.')[0]
-        params_return = params_return.get('return')
-        isprimitive = False
-        params_return_des = {}
-        if params_return is None:
-            api_model = api.model(model_name, {})
-        else:
-            if (not isinstance(params_return, dict) and not inspect.isclass(params_return)):
-                params_return = {'data': params_return}
-                isprimitive = True
-            if(inspect.isclass(params_return)):
-                params_return_des = get_params_description(
-                    params_return.__doc__)
-                params_return = params_return.__annotations__
-            api_model = create_model(
-                params_return, params_return_des)
-            api_model = api.model(model_name, api_model)
-        path_params = ExtractPathParams(path)
-        signature = inspect.signature(func)
-        parameters = dict(signature.parameters)
-        parameters.pop('self')
+def autowire_decorator(func):
+    """The Autowire Decorator that wraps the function"""
 
-        for param in path_params:
-            parameters.pop(param, None)
-        parser = get_parser(signature, parameters)
+    params_return = func.__annotations__
+    model_name = func.__qualname__.split('.')[0]
+    params_return = params_return.get('return')
+    isprimitive = False
+    params_return_des = {}
+    if params_return is None:
+        api_model = api.model(model_name, {})
+    else:
+        if (not isinstance(params_return, dict) and not inspect.isclass(params_return)):
+            params_return = {'data': params_return}
+            isprimitive = True
+        if(inspect.isclass(params_return)):
+            params_return_des = get_params_description(
+                params_return.__doc__)
+            params_return = params_return.__annotations__
+        api_model = create_model(
+            params_return, params_return_des)
+        api_model = api.model(model_name, api_model)
+    signature = inspect.signature(func)
+    parameters = dict(signature.parameters)
+    parameters.pop('self')
 
-        @wraps(func)
-        @api.expect(parser)
-        @api.marshal_with(api_model)
-        def wrapper(*args, **kwargs):
-            args_parser = parser.parse_args()
-            if isprimitive:
-                return {'data': func(*args, **args_parser, **kwargs)}
-            return func(*args, **args_parser, **kwargs)
-        return wrapper
-    return decorator
+    parser = get_parser(signature, parameters)
+
+    @wraps(func)
+    @api.expect(parser)
+    @api.marshal_with(api_model)
+    def wrapper(*args, **kwargs):
+        args_parser = parser.parse_args()
+        if isprimitive:
+            return {'data': func(*args, **args_parser, **kwargs)}
+        return func(*args, **args_parser, **kwargs)
+    return wrapper
