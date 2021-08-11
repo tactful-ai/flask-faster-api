55ccb680fe1dde170b160bc2154106ac97416fd0
Ahmed Ihab <ahmedbaz14@gmail.com>
2021-08-11 16:03:30 +0200

    Fixing GetParams

--- flaskresty/path_param.py
+++ flaskresty/path_param.py
@@ -1,24 +1,23 @@
+'''
+This module is for extracting path params from a given route
+'''
+
 from typing import List
 import re
 
 
-def ExtractPathParams(path):
-    ParamsTypesList: List[str] = []  # Store path params with their type
-    ParamsList: List[str] = []  # Store path params without type
+def extract_path_params(path):
     '''
-    #this code for dealing with List of tuples (the output from ns.resources)
-    urls : List[str]= []
-    #extarct paths related to this API namespace
-    for pathTuple in paths:
-        for path in pathTuple[1]:
-            urls.append(path)
+    Extracting path params from string by regex and append them to list
     '''
+    params_types_list: List[str] = []  # Store path params with their type
+    params_list: List[str] = []  # Store path params without type
     # Searching for params between angle brackets
-    params = re.findall('\<(.*?)\>', path)  # returns a list with all matches
-    if(len(params) > 0):
-        ParamsTypesList.extend(params)
+    params = re.findall(r'\<(.*?)\>', path)  # returns a list with all matches
+    if len(params) > 0:
+        params_types_list.extend(params)
     # removing param type
-    for param in ParamsTypesList:
+    for param in params_types_list:
         param = param[param.find(':')+1:]
-        ParamsList.append(param)
-    return ParamsList
+        params_list.append(param)
+    return params_list
