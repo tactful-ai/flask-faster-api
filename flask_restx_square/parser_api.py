91b2652800b9e40e6120f6958065f9486ad10c6c
omargamal253 <omargamal253@gmail.com>
2021-08-12 06:41:10 +0200

    update verify.sh and verify parser_api by mypy and pylint

--- flask_restx_square/parser_api.py
+++ flask_restx_square/parser_api.py
@@ -1,34 +1,25 @@
+""" This module enable to add parameters to be parsed """
+
 import ast
-from flask_restx import reqparse
+from flask_restx import reqparse  # type: ignore
 
 
-def get_parser(signature, parameters) -> reqparse:
+def get_parser(parameters) -> reqparse:
+    """
+    Parse all parameters which taken from the provided auto-wire decorator and
+    return the results as a parser.
+
+    :param parameters:
+    :return: parser
+    """
     parser = reqparse.RequestParser()
-    location: str = 'args'  # default location -> 'args'
 
     for param in parameters.values():
-        if param.name == "self":
-            continue
-
-        if str(param).find('Query') != -1:
-            location = 'args'
-
-        elif str(param).find('Body') != -1:
-            location = 'form'
-
-        elif str(param).find('Header') != -1:
-            location = 'headers'
-
-        elif str(param).find('Cookie') != -1:
-            location = 'cookies'
-
-        elif str(param).find('Path') != -1:
-            continue
-        else:
-            location = 'args'
-
+        location: str = get_location(param)
         annotation = parameters[str(param.name)].annotation
 
+        if location == 'Path':
+            continue
         if str(annotation).find('typing.List[str]') != -1:
             parser.add_argument(str(param.name), type=str, action='append',
                                 location=location)
@@ -45,7 +36,6 @@ def get_parser(signature, parameters) -> reqparse:
             if res and len(res) > 0:
                 parser.add_argument(str(param.name), type=type(res[0]), required=True, choices=res,
                                     location=location)
-
         else:
             parser.add_argument(str(param.name), type=parameters[str(param.name)].annotation,
                                 location=location)
@@ -54,11 +44,37 @@ def get_parser(signature, parameters) -> reqparse:
 
 
 def get_literal_tuple(annotation):
-    s: int = str(annotation).index('[')
-    e: int = str(annotation).index(']')
-    str_list = str(annotation)[s + 1:e]
+    """
+    get literal option from annotation string and return result in tuple.
+    """
+    s_index: int = str(annotation).index('[')
+    e_index: int = str(annotation).index(']')
+    str_list = str(annotation)[s_index + 1:e_index]
 
     res = ast.literal_eval(str_list)
-
     if res:
         return tuple(res)
+
+    return None
+
+
+def get_location(param) -> str:
+    """
+    take param and return his location.
+    """
+    if str(param).find('Query') != -1:
+        return 'args'
+
+    if str(param).find('Body') != -1:
+        return 'form'
+
+    if str(param).find('Header') != -1:
+        return 'headers'
+
+    if str(param).find('Cookie') != -1:
+        return 'cookies'
+
+    if str(param).find('Path') != -1:
+        return 'Path'
+
+    return 'args'
