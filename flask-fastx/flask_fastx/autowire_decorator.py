0f66f55a728e20c10606cbbb524be410130be2e2
nadaabdelmaboud <42664649+nadaabdelmaboud@users.noreply.github.com>
2021-08-18 19:59:52 +0200

    Add Flask-Fastx

---
+++ flask-fastx/flask_fastx/autowire_decorator.py
@@ -0,0 +1,83 @@
+"""The Autowire decorator is used to
+replace @api.marshal_with decorator and
+@api.expect decorator through supporting MyPy types."""
+import inspect
+from functools import wraps
+from flask_fastx import Api      # type: ignore
+from flask_fastx.model_api import create_model
+from flask_fastx.parser_api import get_parser
+
+
+class ApiDecorator():
+    """The API Decorator"""
+
+    def __init__(self) -> None:
+        """Initialize the API decorator"""
+        self.api = Api()
+
+    def set_api(self, api: Api) -> None:
+        """Set the API"""
+        self.api = api
+
+    def get_api(self) -> Api:
+        """Get the API"""
+        return self.api
+
+
+API = ApiDecorator()
+
+
+def register_api(api_main):
+    """Register The Main App Api"""
+    API.set_api(api_main)
+
+
+def get_params_description(doc):
+    """Get the parameters description from the docstring"""
+    params_description = {}
+    if doc is not None:
+        doc = doc.split('\n')
+        for line in doc:
+            if ':' in line:
+                line = line.split(':')
+                line[0] = line[0].strip()
+                params_description[line[0]] = line[1]
+    return params_description
+
+
+def autowire(func):
+    """The Autowire Decorator that wraps the function"""
+    api = API.get_api()
+    params_return = func.__annotations__
+    model_name = func.__qualname__.split('.')[0]
+    params_return = params_return.get('return')
+    isprimitive = False
+    params_return_des = {}
+    if params_return is None:
+        api_model = api.model(model_name, {})
+    else:
+        if (not isinstance(params_return, dict) and not inspect.isclass(params_return)):
+            params_return = {'data': params_return}
+            isprimitive = True
+        if inspect.isclass(params_return):
+            params_return_des = get_params_description(
+                params_return.__doc__)
+            params_return = params_return.__annotations__
+        api_model = create_model(
+            params_return, params_return_des)
+        api_model = api.model(model_name, api_model)
+    signature = inspect.signature(func)
+    parameters = dict(signature.parameters)
+    parameters.pop('self')
+
+    parser = get_parser(parameters)
+
+    @wraps(func)
+    @api.expect(parser)
+    @api.marshal_with(api_model)
+    def wrapper(*args, **kwargs):
+        args_parser = parser.parse_args()
+        if isprimitive:
+            return {'data': func(*args, **args_parser, **kwargs)}
+        return func(*args, **args_parser, **kwargs)
+    return wrapper
---
+++ flask-fastx/tests/test_model.py
@@ -0,0 +1,29 @@
+# This is the unit test of the create_model function
+# Author: Seif Ashraf
+
+# Testing Methodology
+# send return types example as a dict to the function to simulate the output of the annotations function
+# let create_model() process input and get its output
+# compare its output types with a sample model
+# NOTE: typing.List needs comparing its type of fields.List with the create_model output type cuz idk why
+
+from flask_restx_square import *
+from flask_restx_square.model_api import create_model
+import unittest
+from .utils import *
+
+
+# the unit testing function
+
+descriptions = dict()
+
+
+class TestModel(unittest.TestCase):
+    def test_one(self):
+        self.assertEqual(ModelMatcher(
+            create_model(input, descriptions)), model)
+
+
+if __name__ == '__main__':
+
+    unittest.main()
