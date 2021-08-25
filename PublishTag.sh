a9fdc059dbed2bc3e1ae6aeb9a7c946d9af90b90
ahmedihabb2 <ahmedbaz14@gmail.com>
2021-08-25 14:28:16 +0200

    New Tag

--- PublishTag.sh
+++ PublishTag.sh
@@ -3,5 +3,8 @@
 read -p 'Rule: ' rule
 
 poetry version $rule
-git tag $(poetry version -s)
-git push $(poetry version -s)
+git add .
+git commit -m "New Tag"
+git push
+git tag v$(poetry version -s)
+git push origin v$(poetry version -s)
