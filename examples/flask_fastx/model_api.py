21ca8d2016b729a190095a724449982525c6d65e
Seif Ashraf <63073172+seifashraf1@users.noreply.github.com>
2021-08-25 15:22:02 +0200

    added complex types
    Complex types are now supported. For any response model, each of its attributes can be of complex types like Classes, Classes that have attributes of some Class, and it keeps nesting.....

--- examples/flask_fastx/model_api.py
+++ examples/flask_fastx/model_api.py
@@ -4,6 +4,7 @@ it takes return type as input and returns a model as an output
 '''
 # pylint: disable=C0207
 
+import re
 from flask_restx import fields   # type: ignore
 
 # dict to map each return type to its field data type
@@ -17,33 +18,81 @@ python2restplus = {
     'relation': fields.Nested,
     'typing.List': fields.List,
     'dict': fields.Raw,
+    'nested': fields.Nested,
 }
 
 
-def create_model(types, descriptions):
+def create_model(api, types, descriptions):
     '''
     the model generator function which takes return type as input and returns a model as an output
     '''
-    modeltemp = {}
-    for type_ in types:
-        ans = str(types[type_])
+    x = dict()
 
-        if ans.find('typing') != -1:
-            key = ans.split("[")[0]
-            fields_param = ans.split("[")[1].replace("]", "")
-            type_ = str(type_)
-            if descriptions.get(type_) is None:
-                descriptions[type_] = ""
-            if python2restplus.get(fields_param) is None:
-                fields_param = 'dict'
-            modeltemp[type_] = python2restplus[key](
-                python2restplus[fields_param], description=descriptions[type_])
+    for key, value in types.items():
+        if isinstance(value, dict):
+            x = extract_dict_attr(api, value, key, descriptions)
+            types[key] = fields.Nested(api.model(key, x))
         else:
-            k = str(type_)
-            ans = ans.split(" ")[1]
-            ans = ans.replace("'", "")
-            ans = ans.replace(">", "")
-            if descriptions.get(k) is None:
-                descriptions[k] = ""
-            modeltemp[k] = python2restplus[ans](description=descriptions[k])
-    return modeltemp
+            q = str(value)
+            if q.find('typing') != -1:
+                x, types[key] = extract_prim_typing(
+                    value, key, descriptions)
+            else:
+                x, types[key] = extract_prim(value, key, descriptions)
+    return types
+
+
+def extract_dict_attr(api, ans, type_, descriptions):
+    '''
+    extracts any dictionary attributes to API models, works with primitive and complex types
+    '''
+    fieldTypes = dict()
+    for key, value in ans.items():
+        if isinstance(value, dict):
+            x = extract_dict_attr(api, value, key, descriptions)
+            fieldTypes[key] = fields.Nested(api.model(key, x))
+        else:
+            q = str(value)
+            if q.find('typing') != -1:
+                x, ans = extract_prim_typing(value, key, descriptions)
+                fieldTypes[key] = ans
+            else:
+                x, ans = extract_prim(value, key, descriptions)
+                fieldTypes[key] = ans
+    return fieldTypes
+
+
+def extract_prim_typing(ans, type_, descriptions):
+    ans = str(ans)
+    if ans.find('typing') != -1:
+        key = ans.split("[")[0]
+        fields_param = ans.split("[")[1].replace("]", "")
+        type_ = str(type_)
+        if descriptions.get(type_) is None:
+            descriptions[type_] = ""
+        if python2restplus.get(fields_param) is None:
+            fields_param = 'dict'
+
+        key = str(key)
+
+        fieldsType = python2restplus[key](
+            python2restplus[fields_param], description=descriptions[type_])
+
+        return key, fieldsType
+
+
+def extract_prim(ans, type_, descriptions):
+    ans = str(ans)
+    k = str(type_)
+    ans = ans.split(" ")[1]
+    ans = ans.replace("'", "")
+    ans = ans.replace(">", "")
+    if descriptions.get(k) is None:
+        descriptions[k] = ""
+    key = ans
+    if key in python2restplus:
+        fieldsType = python2restplus[key](description=descriptions[k])
+    else:
+        fieldsType = None
+
+    return key, fieldsType
